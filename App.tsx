4a6bc9d2985cb7b474fdf0cf48d8b66d4d1f2299
Angan Samadder <rio.angan@gmail.com>
2022-11-03 02:10:53 -0400

    Create random picture generator with customizable dimensions

--- App.tsx
+++ App.tsx
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  interface picture {
+    url: string;
+    width: number;
+    height: number;
+  }
+
+  const [image, setImage] = useState<picture>({
+    url: "https://picsum.photos/200/300",
+    width: 200,
+    height: 300,
+  });
+
+  const asyncWidthPrompt = async () =>
+    new Promise<number>((resolve) => {
+      Alert.alert(
+        "Width",
+        "Current Width: " + image.width,
+        [
+          { text: "200", onPress: () => resolve(200) },
+          { text: "300", onPress: () => resolve(300) },
+          { text: "400", onPress: () => resolve(400) },
+        ],
+        { cancelable: false }
+      );
+    });
+
+  const asyncHeightPrompt = async (): Promise<number> =>
+    new Promise<number>((resolve) => {
+      Alert.alert(
+        "Height",
+        "Current Height: " + image.height,
+        [
+          { text: "200", onPress: () => resolve(200) },
+          { text: "300", onPress: () => resolve(300) },
+          { text: "400", onPress: () => resolve(400) },
+        ],
+        { cancelable: false }
+      );
+    });
+
+  const handleDimensions = async () => {
+    const newWidth: number = await asyncWidthPrompt();
+    const newHeight: number = await asyncHeightPrompt();
+    const sameDimensions: boolean =
+      newWidth === image.width && newHeight === image.height;
+    if (sameDimensions) {
+      await handleRefresh();
+    } else {
+      setImage({
+        url: `https://picsum.photos/${newWidth}/${newHeight}`,
+        width: newWidth,
+        height: newHeight,
+      });
+    }
+  };
+
+  const handleRefresh = async () => {
+    const response = await fetch(
+      `https://picsum.photos/${image.width}/${image.height}`
+    );
+
+    setImage({
+      ...image,
+      url: response.url,
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Random Picture Generator</Text>
+      <TouchableHighlight onPress={handleRefresh}>
+        <Image
+          source={{
+            width: image.width,
+            height: image.height,
+            uri: image.url,
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="Change Dimensions"
+        onPress={() =>
+          Alert.alert("Would you like to change dimensions?", "", [
+            {
+              text: "Yes",
+              onPress: handleDimensions,
+            },
+            { text: "No" },
+          ])
+        }
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
